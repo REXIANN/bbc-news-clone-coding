be5441f1efa98a0cb565027a5558c16e83c796a3
REXIANN <gandolkim@gmail.com>
2021-07-11 19:53:41 +0900

    기본 동작 완성

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
-// 스크립트로 스크롤 구간에 따라 그래픽 아이템들의 visible 클래스를 붙였다 뗐다 해주면 된다
-// 각 말풍선과 이미지가 일치해야 하므로 말풍선에 따라 이미지를 정해주면 된다.
-// 그러기 위해서는 이미지와 말풍선의 쌍을 맞춰주어야 한다.
+(() => {
+  const stepElems = document.querySelectorAll('.step');
+  const graphicElems = document.querySelectorAll('.graphic-item');
+  let currentItem; // 현재 활성화된(visible클래스가 불튼) .graphic-item을 지정
+
+  // 각 엘리먼트에 data-index 추가
+  for (let i = 0; i < stepElems.length; i++) {
+    // stepElems[i].setAttribute('data-index', i);
+    stepElems[i].dataset.index = i;
+    graphicElems[i].dataset.index = i;
+  }
+
+  // 각 step의 위치를 파악
+  // 해당 step이 특정 구간에 들어오면 그에 맞는 이미지를 보여준다 
+  window.addEventListener('scroll', () => {
+    let step;
+    let boundingRect;
+  
+    for (let i = 0; i < stepElems.length; i++) {
+      // 엘리먼트의 위치를 가져옴
+      step = stepElems[i];
+      boundingRect = step.getBoundingClientRect();
+
+      // 범위 설정
+      const innerHeightCondition = 
+        boundingRect.top > window.innerHeight * 0.1 && 
+        boundingRect.top < window.innerHeight * 0.8
+      
+      if (innerHeightCondition) {
+        const dataIndex = step.dataset.index;
+
+        if (currentItem) {
+          currentItem.classList.remove('visible')
+        }
+        currentItem = graphicElems[dataIndex]
+        currentItem.classList.add('visible')
+      }
+    }
+  });
+
+})(); 
